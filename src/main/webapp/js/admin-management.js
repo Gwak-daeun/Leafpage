b77ba5564b317e1d52c048073c2093b54e988da1
95Donguk <midgard95@nate.com>
2023-09-11 10:48:42 +0900

    refactor: 도서 관리 페이지 script 링크 변경 및 불필요 파일 삭제

--- src/main/webapp/js/admin-management.js
+++
@@ -1,6 +0,0 @@
-const bookInputEl = document.getElementById("editBook");
-const editEl = document.getElementById("editModal");
-
-editEl.addEventListener("shown.bs.modal", function() {
-  bookInputEl.focus();
-})
