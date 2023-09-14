5d0df4f90a9d49e3295e76992c1cc0a4249bf5e2
soommin0019 <smlee0109@naver.com>
2023-09-14 17:39:35 +0900

    feat: detailPage ui almost

--- src/main/webapp/js/detailPage.js
+++ src/main/webapp/js/detailPage.js
@@ -30,3 +30,10 @@ $(document).ready(function(){
       $("#fullH").hide();
   });
 });
+
+
+$('.starRev span').click(function(){
+  $(this).parent().children('span').removeClass('on');
+  $(this).addClass('on').prevAll('span').addClass('on');
+  return false;
+});
