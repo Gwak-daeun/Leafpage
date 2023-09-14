79d50197247eb7f18c9799b40a8d10276bd18c68
soommin0019 <smlee0109@naver.com>
2023-09-14 22:30:54 +0900

    refactor: 화면 정렬 재수정

---
+++ target/leafpage-1.0-SNAPSHOT/js/detailPage.js
@@ -0,0 +1,39 @@
+$(document).ready(function () {
+  $(".tab-button > li").click(function () {
+    console.log("dhfsldkjfs");
+    var idx = $(this).index();
+
+    $(this).addClass("on").siblings().removeClass("on");
+
+    $(".tabmenu .tab-content")
+      .eq(idx)
+      .addClass("on")
+      .siblings(".tab-content")
+      .removeClass("on");
+  })
+});
+
+$(document).ready(function(){
+  /*웹페이지 열었을 때*/
+  $("#emptyH").show();
+  $("#fullH").hide();
+
+  /*emptyH을 클릭했을 때 fullH를 보여줌*/
+  $("#emptyH").click(function(){
+      $("#emptyH").hide();
+      $("#fullH").show();
+  });
+
+  /*fullH를 클릭했을 때 emptyH을 보여줌*/
+  $("#fullH").click(function(){
+      $("#emptyH").show();
+      $("#fullH").hide();
+  });
+});
+
+
+$('.starRev span').click(function(){
+  $(this).parent().children('span').removeClass('on');
+  $(this).addClass('on').prevAll('span').addClass('on');
+  return false;
+});
