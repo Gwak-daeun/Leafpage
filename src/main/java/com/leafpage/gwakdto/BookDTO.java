5f687a9d77d946a82084b5ef923e7fc5c1f96a48
Gwak-daeun <bbibibbibi22@gmail.com>
2023-09-19 14:04:24 +0900

    refactor(gwakdto/bookDTO): 데이터 베이스 변경으로 인한 필드 dto필드 수정

--- src/main/java/com/leafpage/gwakdto/BookDTO.java
+++ src/main/java/com/leafpage/gwakdto/BookDTO.java
@@ -10,16 +10,18 @@ import lombok.NoArgsConstructor;
 public class BookDTO {
 
     private String ISBN;
-    private int author_no;
-    private String publisher_name;
     private String book_name;
+    private String book_author_name;
     private String book_img;
     private String book_info;
+    private String book_publisher_name;
     private String book_pub_date;
     private String book_upload_date;
     private String book_content;
     private String book_chapter;
     private int book_views;
+    private String book_state;
+
 
     public BookDTO(String  string1, int anInt1, String string2, String string3) {
     }
